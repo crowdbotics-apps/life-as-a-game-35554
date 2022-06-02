af659bbe50b4ae6f631fe2cc322a7a161019ab6b
Crowdbotics <team@crowdbotics.com>
2022-06-02 03:44:39 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "life_as_a_game_35554",
+  url: "https://life_as_a_game_35554.botics.co",
+  api: "https://life_as_a_game_35554.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
